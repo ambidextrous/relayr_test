12c1485c21f0e50d0791ef0bdb7f7f2a1480391b
Aidan Butler <aidan.butler@trustyou.net>
2020-10-18 10:46:37 +0200

    Add product PUT and DELETE unit tests

--- tests/tests_unit/test_handlers.py
+++ tests/tests_unit/test_handlers.py
@@ -3,7 +3,11 @@ from asyncio import sleep
 from unittest import mock
 from datetime import datetime
 
-from product_comparison_service.handlers.handlers import ProductHandler, DocsHandler, DATETIME_FORMAT
+from product_comparison_service.handlers.handlers import (
+    ProductHandler,
+    DocsHandler,
+    DATETIME_FORMAT,
+)
 
 
 class AsyncMock(mock.MagicMock):
@@ -33,7 +37,7 @@ class TestGetProducts:
     async def test_cached_hit(self, mock_get_docs):
 
         # Arrange
-        timestamp = datetime.strftime(datetime.now(),DATETIME_FORMAT)
+        timestamp = datetime.strftime(datetime.now(), DATETIME_FORMAT)
         mock_get_docs.return_value = "test_docs"
 
         mock_self = mock.MagicMock()
@@ -77,12 +81,15 @@ class TestGetProducts:
         # Assert
         mock_self.write.assert_called_once_with(expected_write_value)
 
-    @mock.patch("product_comparison_service.handlers.handlers.search_by_product_or_category", new_callable=AsyncMock)
+    @mock.patch(
+        "product_comparison_service.handlers.handlers.search_by_product_or_category",
+        new_callable=AsyncMock,
+    )
     @pytest.mark.asyncio
     async def test_db_hit(self, mock_search_by_product_or_category):
 
         # Arrange
-        timestamp = datetime.strftime(datetime.now(),DATETIME_FORMAT)
+        timestamp = datetime.strftime(datetime.now(), DATETIME_FORMAT)
         mock_self = mock.MagicMock()
         mock_self.get_argument.side_effect = ["test_product", "test_category"]
         mock_self.cache_dict = {}
@@ -125,8 +132,10 @@ class TestGetProducts:
         # Assert
         mock_self.write.assert_called_once_with(expected_write_value)
 
-
-    @mock.patch("product_comparison_service.handlers.handlers.search_by_product_or_category", new_callable=AsyncMock)
+    @mock.patch(
+        "product_comparison_service.handlers.handlers.search_by_product_or_category",
+        new_callable=AsyncMock,
+    )
     @pytest.mark.asyncio
     async def test_api_hit(self, mock_search_by_product_or_category):
 
@@ -150,8 +159,7 @@ class TestGetProducts:
             }
         ]
         updated_results = {
-            ('DavesPets', 'coyotee'):
-            {
+            ("DavesPets", "coyotee"): {
                 "product": "coyotee",
                 "description": "Oportunistic",
                 "category": "Canines",
@@ -169,17 +177,19 @@ class TestGetProducts:
 
         expected_write_value = {
             "success": True,
-            "search_results": [{
-                "product": "coyotee",
-                "description": "Oportunistic",
-                "category": "Canines",
-                "price": 1,
-                "supplier": "DavesPets",
-                "product_rating": 0.4,
-                "supplier_rating": 0.8,
-                "combined_rating": 0.6,
-                "last_updated": "2020-10-10T09:22:37.398697",
-            }],
+            "search_results": [
+                {
+                    "product": "coyotee",
+                    "description": "Oportunistic",
+                    "category": "Canines",
+                    "price": 1,
+                    "supplier": "DavesPets",
+                    "product_rating": 0.4,
+                    "supplier_rating": 0.8,
+                    "combined_rating": 0.6,
+                    "last_updated": "2020-10-10T09:22:37.398697",
+                }
+            ],
         }
 
         # Act
@@ -188,3 +198,77 @@ class TestGetProducts:
         # Assert
         mock_self.write.assert_called_once_with(expected_write_value)
 
+
+class TestDeleteProduct:
+    @mock.patch(
+        "product_comparison_service.handlers.handlers.delete_supplier_product_data",
+        new_callable=AsyncMock,
+    )
+    @pytest.mark.asyncio
+    async def test_delete(self, mock_delete_supplier_product_data):
+
+        # Arrange
+        timestamp = datetime.strftime(datetime.now(), DATETIME_FORMAT)
+        mock_self = mock.MagicMock()
+        mock_self.get_argument.side_effect = ["test_product", "test_category"]
+        mock_self.get_async_conn_and_cur = mock.AsyncMock()
+        mock_self.get_async_conn_and_cur.return_value = ("test_conn", "test_cur")
+
+        expected_write_value = {
+            "success": True,
+            "deleted": {"product": "test_product", "supplier": "test_category"},
+        }
+
+        # Act
+        await ProductHandler.delete(mock_self)
+
+        # Assert
+        mock_delete_supplier_product_data.assert_called_once_with(
+            "test_conn", "test_cur", "test_product", "test_category"
+        )
+        mock_self.write.assert_called_once_with(expected_write_value)
+
+
+class TestPutProduct:
+    @mock.patch("product_comparison_service.handlers.handlers.datetime")
+    @mock.patch(
+        "product_comparison_service.handlers.handlers.update_supplier_product_data",
+        new_callable=AsyncMock,
+    )
+    @pytest.mark.asyncio
+    async def test_delete(self, mock_update_supplier_product_data, mock_datetime):
+
+        # Arrange
+        timestamp = datetime.strftime(datetime.now(), DATETIME_FORMAT)
+        mock_datetime.strftime.return_value = timestamp
+        mock_self = mock.MagicMock()
+        mock_self.get_argument.side_effect = [
+            "test_product",
+            "test_desciption",
+            "test_category",
+            "test_price",
+            "test_supplier",
+            "test_product_rating",
+        ]
+        mock_self.get_async_conn_and_cur = mock.AsyncMock()
+        mock_self.get_async_conn_and_cur.return_value = ("test_conn", "test_cur")
+
+        expected_write_value = {
+            "success": True,
+            "upserted": {
+                "product": "test_product",
+                "description": "test_desciption",
+                "category": "test_category",
+                "price": "test_price",
+                "supplier": "test_supplier",
+                "product_rating": "test_product_rating",
+                "last_updated": timestamp,
+            },
+        }
+
+        # Act
+        await ProductHandler.put(mock_self)
+
+        # Assert
+        mock_update_supplier_product_data.assert_called_once()
+        mock_self.write.assert_called_once_with(expected_write_value)
